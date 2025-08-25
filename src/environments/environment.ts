6ff32c5520c04bcc593556dd70281e4d156e1ba8
alexis.rueda <alexisrueda34@gmail.com>
2025-08-25 01:17:35 -0600

    Agregar interfaces para Giphy y configurar el cliente HTTP en la aplicación

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,7 @@ export const environment = {
   companySlogan: 'Maneja tus gifs',
 
   // ApiKeys
-
+  giphyApiKey: 'p8e22evcAQpc2aX4liwyvt9OWItAy4Yo',
   // URLS
+  giphyUrl: 'https://api.giphy.com/v1',
 };
